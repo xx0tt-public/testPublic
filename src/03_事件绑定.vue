01ab570244b5a56c159d659f2fa9639d5d84f615
renbuairen <1664844124@qq.com>
2022-06-28 20:58:16 +0800

    事件绑定

--- src/03_事件绑定.vue
+++ src/03_事件绑定.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
-    <p>你要购买商品的数量: </p>
-    <button>+1</button>
-    <button>+1</button>
-    <button>+5</button>
-    <button>减少1</button>
+    <p>商品总数{{ count }}</p>
+    <button @click="count++">+1</button>
+    <button @click="addFn">+1</button>
+    <button @click="addFn1(5)">+5</button>
+    <button @click="subFn">-1</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'VuecliDemo03',
-
+  name: 'App',
   data() {
     return {
-      
-    };
+      count: 0,
+    }
   },
-
-  mounted() {
-    
-  },
-
   methods: {
-    
+    addFn() {
+      this.count++
+    },
+    addFn1(value) {
+      this.count += value
+    },
+    subFn() {
+      this.count--
+    },
   },
-};
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style></style>
