53b98565085ae7a85e7b5fb8b252e8e70650e3fa
renbuairen <1664844124@qq.com>
2022-06-28 20:55:54 +0800

    动态属性

--- src/02_动态属性.vue
+++ src/02_动态属性.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
-    
+    <a v-bind:href="href">百度</a>
+    <img v-bind:src="imgUrl" />
+    <img :src="imgLocal" />
+    <img src="./assets/logo.png" />
   </div>
 </template>
 
 <script>
-// http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg
+import imgLocal from './assets/logo.png'
+// 唯独js需要导入图片文件(css/标签里可以直接使用图片地址)
 export default {
-  name: 'VuecliDemo02',
-
+  name: 'app',
   data() {
     return {
-      
-    };
+      href: 'http://www.baidu.com/',
+      imgUrl:
+        'http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg',
+      imgLocal: imgLocal,
+    }
   },
-
-  mounted() {
-    
-  },
-
-  methods: {
-    
-  },
-};
+}
 </script>
+
+<style></style>
