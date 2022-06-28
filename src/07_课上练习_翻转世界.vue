650e7ede35e55f1e31932ecb04aff2b2de75f672
renbuairen <1664844124@qq.com>
2022-06-28 21:02:54 +0800

    反转世界

--- src/07_课上练习_翻转世界.vue
+++ src/07_课上练习_翻转世界.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-    <h1></h1>
-    <button>逆转世界</button>
+    <h1>{{ msg }}</h1>
+    <button @click="clickFn">翻转</button>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  name: 'App',
+  data() {
     return {
+      msg: 'Hello, world!',
     }
   },
   methods: {
-  }
+    clickFn() {
+      this.msg = this.msg.split('').reverse().join('')
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
