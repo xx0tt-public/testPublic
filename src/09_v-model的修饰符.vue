a9f94346b245140356bb163155d44ced2378ea3f
renbuairen <1664844124@qq.com>
2022-06-28 21:10:43 +0800

    model的修饰符

--- src/09_v-model的修饰符.vue
+++ src/09_v-model的修饰符.vue
@@ -1,15 +1,14 @@
-
 <template>
   <div>
     <div>
       <span>年龄</span>
       <!-- .number修饰符-把值parseFloat转数值再赋予给v-model对应的变量 -->
-      <input type="text" v-model.number="age">
+      <input type="text" v-model.number="age" />
     </div>
     <div>
       <!-- .trim修饰 - 去除首尾两边空格 -->
       <span>人生格言</span>
-      <input type="text" v-model.trim="motto">
+      <input type="text" v-model.trim="motto" />
     </div>
     <div>
       <!-- .lazy修饰符 - 失去焦点内容改变时(onchange事件), 把内容同步给v-model的变量 -->
@@ -21,16 +20,14 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      age: 0,
-      motto: "",
-      intro: ""
+      age: 18,
+      motto: '',
+      intro: '',
     }
-  }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
