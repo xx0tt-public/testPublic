3cabf8a401c866037adfb8fdfede718c70aabe5a
renbuairen <1664844124@qq.com>
2022-06-28 21:07:18 +0800

    模拟实现v-model

--- src/08_01_v-model的基本使用.vue
+++ src/08_01_v-model的基本使用.vue
@@ -2,29 +2,32 @@
   <div>
     <div>
       <span>用户名:</span>
-      <!-- 1. v-model
-         双向数据绑定
-         value属性 - vue变量
-       -->
-      <input type="text" >
+      <input type="text" v-model="text" />
     </div>
     <div>
       <span>密码: </span>
-      <input type="password">
+      <input type="password" v-model="password" />
     </div>
+    <input type="text" v-bind:value="val1" @input="inputFn" />
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-
+      text: '',
+      password: '',
+      val: '',
+      val1: '',
     }
-  }
+  },
+  methods: {
+    inputFn(e) {
+      this.val1 = e.target.value
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
